37267c71d6034a6e560511e401d73bee4f5b88e5
Fady Fouad <fady.fouad.a@gmail.com>
2019-12-28 15:25:46 +0200

    Question widget style

--- lib/question.dart
+++ lib/question.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
-  String questionText;
+  final String questionText;
 
   Question({this.questionText});
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      margin: EdgeInsets.all(8),
+      width: double.infinity,
+      child: Text(
+        questionText,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
